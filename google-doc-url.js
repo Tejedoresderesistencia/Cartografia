d861ca6a4a289d7c4d8308309c5bb96d6bbdcd3e
Tejedoresderesistencia <84676684+Tejedoresderesistencia@users.noreply.github.com>
2021-05-23 11:22:08 -0500

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1lmVxAG9fFhRZ001G_isrTsWFiCWsKm6RaYzz1oqgxwE/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-
+var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
